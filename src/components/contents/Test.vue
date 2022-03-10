c53fdeab1be7955311f2de2f9588c38e84435f06
hwonkim <hwonkim@crscube.co.kr>
2022-03-10 15:55:32 +0900

    dropdown 추가 테스트

--- src/components/contents/Test.vue
+++ src/components/contents/Test.vue
@@ -6,12 +6,27 @@
   <b-toast id="test-toast" title="Test" static no-auto-hide>
     Hello...
   </b-toast>
+  <br>
+  <b-dropdown v-bind:text="curData">
+    <b-dropdown-item @click="select">DAAATAAAA1</b-dropdown-item>
+    <b-dropdown-item @click="select">TTTTTTTTT2</b-dropdown-item>
+  </b-dropdown>
 </div>
 </template>
 
 <script>
 export default {
-  name: "Test"
+  name: "Test",
+  data() {
+    return {
+      curData: "입력해주세요",
+    }
+  },
+  methods: {
+    select(event){
+      this.curData = event.target.innerText;
+    }
+  }
 }
 </script>
 
